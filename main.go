bf345f5f94cc8e9d90a4ecc29936d22e38941f20
= <=>
2022-02-27 22:27:13 -0600

    added source options and handling

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/gmcalab/parser/grafana"
@@ -12,18 +14,37 @@ import (
 func main() {
 
 	if len(os.Args) <= 1 {
-		panic("Missing local path to dashboard yaml file.")
+		usage()
 	}
 
-	dashboardYaml := os.Args[1]
-
-	metrics := make([]string, 0, 20)
-
 	var dashboard grafana.Dashboard
+	var data []byte
 
-	data := util.ReadFile(dashboardYaml)
+	source := os.Args[1]
+
+	if source == "-h" {
+		help()
+		os.Exit(0)
+	}
+
+	if source != "-p" && source != "-u" {
+		usage()
+	}
+
+	target := os.Args[2]
+
+	if source == "-p" {
+		data = util.ReadFile(target)
+	} else {
+		response, err := http.Get(target)
+		util.CheckError(err)
+		data, err = ioutil.ReadAll(response.Body)
+		util.CheckError(err)
+	}
 
 	json.Unmarshal(data, &dashboard)
+
+	metrics := make([]string, 0, 20)
 
 	grafana.Unpack(dashboard.Panels, &metrics)
 
@@ -36,4 +57,14 @@ func main() {
 	for _, metric := range uniqueMetrics {
 		fmt.Println(metric)
 	}
+}
+
+func help() {
+	fmt.Println("-h\t help")
+	fmt.Println("-p <path>\t file path for dashboard json")
+	fmt.Println("-u <url>\t url for dashboard json")
+}
+
+func usage() {
+	panic("Incorrect usage, please use -h for help")
 }
